aa51eb3764ecc0ccfa59d96e41b995b711489836
Ron <liurongxuan@yit.com>
2017-05-17 19:00:31 +0800

    begin New segementview

---
+++ RRCMS/RRStickSegmentView.h
@@ -0,0 +1,13 @@
+//
+//  RRStickSegmentView.h
+//  RRCMS
+//
+//  Created by Ron on 5/17/17.
+//  Copyright © 2017 Ron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RRStickSegmentView : UICollectionView
+
+@end
---
+++ RRCMS/RRStickSegmentView.m
@@ -0,0 +1,25 @@
+//
+//  RRStickSegmentView.m
+//  RRCMS
+//
+//  Created by Ron on 5/17/17.
+//  Copyright © 2017 Ron. All rights reserved.
+//
+
+#import "RRStickSegmentView.h"
+
+@implementation RRStickSegmentView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
